b19f435e67073f07f733a36d753cabde6d93e900
Ben Ropa <ben@Bropas-MacBook-Pro.local>
2022-12-09 15:25:29 -0600

    prep notes added with tutor

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
 // Assignment code here
 
+// 1. ask for how many characters to use on the password
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+}
 
 // Write password to the #password input
 function writePassword() {
