bad1e04c77b11365d0001957a54778ee63a52d60
Ben Ropa <ben@Bropas-MacBook-Pro.local>
2022-12-10 21:15:31 -0600

    tidying up

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
 // Get references to the #generate element
-
+// Set variables for all characters we will need. Separate into categories for easier concatenation.
 var specChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "-", "_", "/", "?", "<", ">", ".", ",", "[", "]", "{", "}", ":", ";", "~", "`"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
 
 function generatePassword() {
   var selections = [];
